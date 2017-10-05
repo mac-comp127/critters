570648fa8105205059239c2602d8f9ca67812e03
CKunhardt <24also20@gmail.com>
2017-10-04 21:26:33 -0500

    Add ya boi mr. orange with some funky legaroonis

---
+++ src/edu/macalester/comp124/critters/MrOrange.java
@@ -0,0 +1,60 @@
+package edu.macalester.comp124.critters;
+
+import comp124graphics.*;
+import comp124graphics.Rectangle;
+
+import java.awt.Color;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+public class MrOrange extends Critter {
+
+    private static final Color STROKE_COLOR = new Color(233, 159, 121);
+    private static final Color FILL_COLOR = STROKE_COLOR;
+
+    @Override
+    protected void buildGraphics(){
+
+        try {
+            Image rightLeg = new Image(67,97, Paths.get(getClass().getResource("/OrangeRightLeg.png").toURI()).toString());
+            Image leftLeg = new Image(8,87, Paths.get(getClass().getResource("/OrangeLeftLeg.png").toURI()).toString());
+            //getGraphics().add(rightLeg);
+            //getGraphics().add(leftLeg);
+            addLeg(new Leg(rightLeg, 5));
+            addLeg(new Leg(leftLeg, 5));
+            //makeRectLegs();
+        } catch (URISyntaxException e){
+            e.printStackTrace();
+            makeRectLegs();
+        }
+
+        Image orange = null;
+        try {
+            orange = new Image(0,0, Paths.get(getClass().getResource("/Orange.png").toURI()).toString());
+        } catch (URISyntaxException e){
+            e.printStackTrace();
+        }
+
+        getGraphics().add(orange);
+
+        addEye(new Eye(7, 0.5, 0.25, Color.BLUE), 57, 35);
+        addEye(new Eye(7.5, 0.5, 0.25, Color.BLUE), 86, 36);
+
+    }
+
+    private void makeRectLegs(){
+        Rectangle rightLeg = new Rectangle(97,92,20,50);
+        rightLeg.setStrokeColor(STROKE_COLOR);
+        rightLeg.setFilled(true);
+        rightLeg.setFillColor(FILL_COLOR);
+
+        Rectangle leftLeg = new Rectangle(60,92,20,50);
+        leftLeg.setStrokeColor(STROKE_COLOR);
+        leftLeg.setFilled(true);
+        leftLeg.setFillColor(FILL_COLOR);
+
+        addLeg(new Leg(rightLeg, 10));
+        addLeg(new Leg(leftLeg, 10));
+    }
+
+}
