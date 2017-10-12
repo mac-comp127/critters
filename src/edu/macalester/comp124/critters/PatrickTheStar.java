3cb7e011d503bf12b07b1ca798c9e0efd287c0bf
kgiangnguyen <knguyen1@macalester.edu>
2017-10-11 23:37:17 -0500

    Replaced correct formula for getSquareArea Replaced correct formula for getCircleArea

--- src/edu/macalester/comp124/critters/PatrickTheStar.java
+++ src/edu/macalester/comp124/critters/PatrickTheStar.java
@@ -9,19 +9,19 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
 public class PatrickTheStar extends Critter {
-    private static final Paint strokeColor = new Color(156, 0, 0);
+    private static final Paint strokeColor = new Color(156, 100, 100);
     //private static final Color fillColor = new Color(100, 0,0);
     private static final Paint fillColor = strokeColor;
 
     @Override
     protected void buildGraphics() {
-        Rectangle leftLeg  = new Rectangle(70, 245, 20, 50);
+        Rectangle leftLeg  = new Rectangle(70, 215, 20, 100);
         leftLeg.setStrokeColor(strokeColor);
         leftLeg.setFilled(true);
         leftLeg.setFillColor(fillColor);
         addLeg(new Leg(leftLeg, 10));       // this leg will be animated when running CritterProgram
 
-        Rectangle rightLeg  = new Rectangle(110, 245, 20, 50);
+        Rectangle rightLeg  = new Rectangle(110, 215, 20, 100);
         rightLeg.setStrokeColor(strokeColor);
         rightLeg.setFilled(true);
         rightLeg.setFillColor(fillColor);
@@ -30,13 +30,13 @@ public class PatrickTheStar extends Critter {
 
         Image patrick = null;
         try{
-            patrick = new Image(0,0, Paths.get(getClass().getResource("/Patrick_Star.png").toURI()).toString());
+            patrick = new Image(0,0, Paths.get(getClass().getResource("/PatrickTheStar.png").toURI()).toString());
         } catch (URISyntaxException e){
             e.printStackTrace();
         }
         getGraphics().add(patrick);
 
-        addEye(new Eye(15, 0.5, 0.18, Color.WHITE), 95, 60);
-        addEye(new Eye(15, 0.5, 0.18, Color.WHITE), 125, 60);
+        addEye(new Eye(15, 0.5, 0.18, Color.RED), 95, 60);
+        addEye(new Eye(15, 0.5, 0.18, Color.RED), 125, 60);
     }
 }
