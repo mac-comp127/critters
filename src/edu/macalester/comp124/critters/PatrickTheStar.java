134aae854de8744ec7a434357f49a927de3ec8c1
kgiangnguyen <knguyen1@macalester.edu>
2017-10-11 22:58:49 -0500

    Finish Critters lab

---
+++ src/edu/macalester/comp124/critters/PatrickTheStar.java
@@ -0,0 +1,42 @@
+package edu.macalester.comp124.critters;
+
+import comp124graphics.*;
+import comp124graphics.Image;
+import comp124graphics.Rectangle;
+
+import java.awt.*;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+public class PatrickTheStar extends Critter {
+    private static final Paint strokeColor = new Color(156, 0, 0);
+    //private static final Color fillColor = new Color(100, 0,0);
+    private static final Paint fillColor = strokeColor;
+
+    @Override
+    protected void buildGraphics() {
+        Rectangle leftLeg  = new Rectangle(70, 245, 20, 50);
+        leftLeg.setStrokeColor(strokeColor);
+        leftLeg.setFilled(true);
+        leftLeg.setFillColor(fillColor);
+        addLeg(new Leg(leftLeg, 10));       // this leg will be animated when running CritterProgram
+
+        Rectangle rightLeg  = new Rectangle(110, 245, 20, 50);
+        rightLeg.setStrokeColor(strokeColor);
+        rightLeg.setFilled(true);
+        rightLeg.setFillColor(fillColor);
+        addLeg(new Leg(rightLeg, 10));
+
+
+        Image patrick = null;
+        try{
+            patrick = new Image(0,0, Paths.get(getClass().getResource("/Patrick_Star.png").toURI()).toString());
+        } catch (URISyntaxException e){
+            e.printStackTrace();
+        }
+        getGraphics().add(patrick);
+
+        addEye(new Eye(15, 0.5, 0.18, Color.WHITE), 95, 60);
+        addEye(new Eye(15, 0.5, 0.18, Color.WHITE), 125, 60);
+    }
+}
