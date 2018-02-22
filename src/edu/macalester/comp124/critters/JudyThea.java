bb6107984ef31539d8180debc9b279106f872075
theang66 <anguyen1@macalester.edu>
2018-02-22 14:22:04 -0600

    Critter Lab

--- src/edu/macalester/comp124/critters/JudyThea.java
+++ src/edu/macalester/comp124/critters/JudyThea.java
@@ -11,31 +11,31 @@ import java.nio.file.Paths;
  * Created by Thea Nguyen on 02/22/2018.
  */
 
-public class TheaCritter extends Critter {
+public class JudyThea extends Critter {
     @Override
     protected void buildGraphics() {
-        Rectangle leftLeg  = new Rectangle(10, 80, 17, 10);
-        leftLeg.setStrokeColor(Color.BLUE);
+        Rectangle leftLeg  = new Rectangle(10, 105, 17, 3);
+        leftLeg.setStrokeColor(Color.BLACK);
         leftLeg.setFilled(true);
-        leftLeg.setFillColor(Color.BLUE);
+        leftLeg.setFillColor(Color.BLACK);
         addLeg(new Leg(leftLeg, 10));       // this leg will be animated when running CritterProgram
 
-        Rectangle rightLeg  = new Rectangle(53, 100, 17, 10);
-        rightLeg.setStrokeColor(Color.BLUE);
+        Rectangle rightLeg  = new Rectangle(44, 105, 17, 3);
+        rightLeg.setStrokeColor(Color.BLACK);
         rightLeg.setFilled(true);
-        rightLeg.setFillColor(Color.BLUE);
+        rightLeg.setFillColor(Color.BLACK);
         addLeg(new Leg(rightLeg, 10));     // this leg will be animated when running CritterProgram
 
-        comp124graphics.Image stitch = null;
+        comp124graphics.Image judy = null;
         try{
-            stitch = new Image(0,0, Paths.get(getClass().getResource("/Stitch.png").toURI()).toString());
+            judy = new Image(0,0, Paths.get(getClass().getResource("/Judy.png").toURI()).toString());
         } catch (URISyntaxException e){
             e.printStackTrace();
         }
 
-        getGraphics().add(stitch);
+        getGraphics().add(judy);
 
-        addEye(new Eye(5, 0.26, 0.18, Color.BLACK), 34, 38);
-        addEye(new Eye(5, 0.26, 0.18, Color.BLACK), 57, 33);
+        addEye(new Eye(3, 0.26, 0.18, Color.BLACK), 34, 36);
+        addEye(new Eye(3, 0.26, 0.18, Color.BLACK), 44, 34);
     }
 }
--- src/edu/macalester/comp124/critters/StitchThea.java
+++ src/edu/macalester/comp124/critters/StitchThea.java
@@ -11,31 +11,31 @@ import java.nio.file.Paths;
  * Created by Thea Nguyen on 02/22/2018.
  */
 
-public class TheaCritter2 extends Critter {
+public class StitchThea extends Critter {
     @Override
     protected void buildGraphics() {
-        Rectangle leftLeg  = new Rectangle(10, 105, 17, 3);
-        leftLeg.setStrokeColor(Color.BLACK);
+        Rectangle leftLeg  = new Rectangle(10, 80, 17, 10);
+        leftLeg.setStrokeColor(Color.BLUE);
         leftLeg.setFilled(true);
-        leftLeg.setFillColor(Color.BLACK);
+        leftLeg.setFillColor(Color.BLUE);
         addLeg(new Leg(leftLeg, 10));       // this leg will be animated when running CritterProgram
 
-        Rectangle rightLeg  = new Rectangle(44, 105, 17, 3);
-        rightLeg.setStrokeColor(Color.BLACK);
+        Rectangle rightLeg  = new Rectangle(53, 100, 17, 10);
+        rightLeg.setStrokeColor(Color.BLUE);
         rightLeg.setFilled(true);
-        rightLeg.setFillColor(Color.BLACK);
+        rightLeg.setFillColor(Color.BLUE);
         addLeg(new Leg(rightLeg, 10));     // this leg will be animated when running CritterProgram
 
-        comp124graphics.Image judy = null;
+        comp124graphics.Image stitch = null;
         try{
-            judy = new Image(0,0, Paths.get(getClass().getResource("/Judy.png").toURI()).toString());
+            stitch = new Image(0,0, Paths.get(getClass().getResource("/Stitch.png").toURI()).toString());
         } catch (URISyntaxException e){
             e.printStackTrace();
         }
 
-        getGraphics().add(judy);
+        getGraphics().add(stitch);
 
-        addEye(new Eye(3, 0.26, 0.18, Color.BLACK), 34, 36);
-        addEye(new Eye(3, 0.26, 0.18, Color.BLACK), 44, 34);
+        addEye(new Eye(5, 0.26, 0.18, Color.BLACK), 34, 38);
+        addEye(new Eye(5, 0.26, 0.18, Color.BLACK), 57, 33);
     }
 }
