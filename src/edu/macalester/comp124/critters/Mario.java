162268788c09ef140fb9bfe0da0f66db1df7a059
Paul Cantrell <cantrell@pobox.com>
2018-10-09 12:46:09 -0500

    Spruced up Mario

--- src/edu/macalester/comp124/critters/Mario.java
+++ src/edu/macalester/comp124/critters/Mario.java
@@ -4,46 +4,31 @@ import comp124graphics.*;
 import comp124graphics.Rectangle;
 
 import java.awt.Color;
-import java.net.URISyntaxException;
-import java.nio.file.Paths;
 
 
 public class Mario extends Critter {
 	private static final Color strokeColor = new Color(0, 0, 255);
-	//private static final Color fillColor = new Color(100, 0,0);
 	private static final Color fillColor = strokeColor;
-
 
 	@Override
 	protected void buildGraphics() {
 
-		Rectangle leftLeg  = new Rectangle(27, 100, 17, 35);
+		Rectangle leftLeg  = new Rectangle(21, 73.5, 12.5, 25.7);
         leftLeg.setStrokeColor(strokeColor);
         leftLeg.setFilled(true);
         leftLeg.setFillColor(fillColor);
-        addLeg(new Leg(leftLeg, 10));       // this leg will be animated when running CritterProgram
+        addLeg(new Leg(leftLeg, 4));
 
-        Rectangle rightLeg  = new Rectangle(50, 100, 17, 35);
+        Rectangle rightLeg  = new Rectangle(35, 73.5, 12.5, 25.7);
         rightLeg.setStrokeColor(strokeColor);
         rightLeg.setFilled(true);
         rightLeg.setFillColor(fillColor);
-        addLeg(new Leg(rightLeg, 10));     // this leg will be animated when running CritterProgram
+        addLeg(new Leg(rightLeg, 4));
 
-
-		Image mario = null;
-		try{
-			mario = new Image(0,0, Paths.get(getClass().getResource("/Mario.png").toURI()).toString());
-		} catch (URISyntaxException e){
-			e.printStackTrace();
-		}
-
-
+		Image mario = CritterUtils.loadCritterImage("Mario.png");
 		getGraphics().add(mario);
 
-
-		addEye(new Eye(5, 0.26, 0.18, Color.RED), 42, 35);
-        addEye(new Eye(5, 0.26, 0.18, Color.RED), 55, 35);
+		addEye(new Eye(3.6, 0.26, 0.18, Color.RED), 31, 26);
+        addEye(new Eye(3.6, 0.26, 0.18, Color.RED), 40, 26);
 	}
-
-
 }
