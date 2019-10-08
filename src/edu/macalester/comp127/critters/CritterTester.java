06268db6fdc626f0c37a86f9e23861f8a860732e
Paul Cantrell <cantrell@pobox.com>
2019-10-08 16:39:39 -0500

    No more subclassing CanvasWindow

--- src/edu/macalester/comp127/critters/CritterTester.java
+++ src/edu/macalester/comp127/critters/CritterTester.java
@@ -12,17 +12,18 @@ import java.awt.geom.Point2D;
 /**
  * Created by shoop on 2/22/16.
  */
-public class CritterTester extends CanvasWindow {
+public class CritterTester {
 
+    private CanvasWindow canvas;
     private Critter testCritter;
 
     public CritterTester() {
-        super("Critter Test", 260, 260);
+        canvas = new CanvasWindow("Critter Test", 260, 260);
 
         Rectangle targetBounds = new Rectangle(40, 40, 100, 100);
         targetBounds.setStrokeColor(new Color(0, 0, 0, 0.1f));
         targetBounds.setStrokeWidth(3);
-        add(targetBounds);
+        canvas.add(targetBounds);
 
         testCritter = new BoxBot();
        // testCritter = new RoundBug();  // try these too
@@ -36,7 +37,7 @@ public class CritterTester extends CanvasWindow {
     private void showCritter() {
         GraphicsGroup graphics = testCritter.getGraphics();
         graphics.setPosition(40.0 + testCritter.getxOffset(), 40.0 + testCritter.getyOffset());
-        add(graphics);
+        canvas.add(graphics);
     }
 
     @SuppressWarnings("InfiniteLoopStatement")
@@ -50,12 +51,12 @@ public class CritterTester extends CanvasWindow {
                     center.getY() + Math.sin(t) * 5 + 5));
             testCritter.moveTowardsGoal(0.05);
 
-            //pause(50);
+            canvas.pause(50);
             t = (t + 0.1) % (Math.PI * 2);
         }
     }
 
     public static void main(String[] args) {
-        CritterTester test = new CritterTester();
+        new CritterTester();
     }
 }
